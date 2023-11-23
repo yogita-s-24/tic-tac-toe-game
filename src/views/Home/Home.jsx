4ad02ba64ca3cd90fe27e13d959da7e1e7917b1f
yogita-s-24 <yogitashete24@gmail.com>
2023-11-23 18:05:03 +0530

    ref :  Tic Tac Toe layout in React component Home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,28 +2,46 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="flex justify-around flex-wrap mt-20">
+    <div className="flex justify-around flex-wrap mt-10">
       <div>
-        <h1 className="text-3xl font-bold font-sans md:font-serif text-black">
+        <h1 className="text-4xl font-bold font-sans md:font-serif  px-20 py-3 rounded-sm bg-violet-500 text-white shadow-3xl" >
           Tic Tac Toe
         </h1>
+
+        <div className="flex justify-around mt-14">
+        <h1 className="text-xl font-black bg-violet-100 px-9 py-3 rounded-sm">Player 1 : ❌</h1>
+        <h1 className="text-xl font-black bg-violet-100 px-9 py-3 ms-1 rounded-sm">Player 2 : ⭕ </h1>
+        </div>
+
       </div>
 
-      <div className="w-fit p-4 bg-amber-800 mt-14">
-        <div className="box flex align-center justify-center ">
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
+      <div className="mt-20">
+        <div className="w-fit p-4 bg-violet-500 rounded-sm shadow-sm">
+          <div className="box flex align-center justify-center ">
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+          </div>
+
+          <div className="box flex align-center justify-center">
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+          </div>
+
+          <div className="box flex align-center justify-center">
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+            <div className="box bg-violet-100 w-28 h-28 border border-violet-700 hover:shadow-3xl hover:z-50 hover:bg-white"></div>
+          </div>
         </div>
-        <div className="box flex align-center justify-center">
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-        </div>
-        <div className="box flex align-center justify-center">
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
-          <div className="box bg-gray-100 w-28 h-28 border border-amber-700 hover:shadow-3xl hover:z-50"></div>
+
+        <div className="text-center mt-7">
+          <button
+            type="button"
+            className="bg-violet-600 text-white px-14 py-2 rounded-sm shadow-3xl">
+            Reset
+          </button>
         </div>
       </div>
     </div>
